8bd3e94ae85f0a6f577c31c93e54e2cd301d1733
slimedalimea <100090330+slimedalimea@users.noreply.github.com>
2023-02-26 17:58:07 +0000

    Update ok.c

--- ok.c
+++ ok.c
@@ -1,22 +1,118 @@
-#include <flipper.h>
-int led_red_pin = 25;   // choose pin 25 for the red LED
-int led_green_pin = 27; // choose pin 27 for the green LED
-int led_blue_pin = 29;  // choose pin 29 for the blue LED
+#include <furi.h>
+#include <gui/gui.h>
+#include <input/input.h>
+#include <stdlib.h>
 
-/* Declare a function to blink the RGB LED */
-void blink_led(void) {
-    while (1) { // loop indefinitely
-        lf_set_pin_state(led_red_pin, 1);   // turn on the red LED
-        lf_set_pin_state(led_green_pin, 0); // turn off the green LED
-        lf_set_pin_state(led_blue_pin, 0);  // turn off the blue LED
-        lf_delay(500); // wait for 500 milliseconds
-        lf_set_pin_state(led_red_pin, 0);   // turn off the red LED
-        lf_set_pin_state(led_green_pin, 1); // turn on the green LED
-        lf_set_pin_state(led_blue_pin, 0);  // turn off the blue LED
-        lf_delay(500); // wait for 500 milliseconds
-        lf_set_pin_state(led_red_pin, 0);   // turn off the red LED
-        lf_set_pin_state(led_green_pin, 0); // turn off the green LED
-        lf_set_pin_state(led_blue_pin, 1);  // turn on the blue LED
-        lf_delay(500); // wait for 500 milliseconds
+typedef enum {
+    EventTypeTick,
+    EventTypeKey,
+} EventType;
+
+typedef struct {
+    EventType type;
+    InputEvent input;
+} PluginEvent;
+
+typedef struct {
+    int x;
+    int y;
+} PluginState;
+
+static void render_callback(Canvas* const canvas, void* ctx) {
+    const PluginState* plugin_state = acquire_mutex((ValueMutex*)ctx, 25);
+    if(plugin_state == NULL) {
+        return;
     }
+    // border around the edge of the screen
+    canvas_draw_frame(canvas, 0, 0, 128, 64);
+
+    canvas_set_font(canvas, FontPrimary);
+    canvas_draw_str_aligned(
+        canvas, plugin_state->x, plugin_state->y, AlignRight, AlignBottom, "hi");
+
+    release_mutex((ValueMutex*)ctx, plugin_state);
+}
+
+static void input_callback(InputEvent* input_event, FuriMessageQueue* event_queue) {
+    furi_assert(event_queue);
+
+    PluginEvent event = {.type = EventTypeKey, .input = *input_event};
+    furi_message_queue_put(event_queue, &event, FuriWaitForever);
+}
+
+static void hello_world_state_init(PluginState* const plugin_state) {
+    plugin_state->x = 50;
+    plugin_state->y = 30;
+}
+
+int32_t hello_world_app() {
+    FuriMessageQueue* event_queue = furi_message_queue_alloc(8, sizeof(PluginEvent));
+
+    PluginState* plugin_state = malloc(sizeof(PluginState));
+
+    hello_world_state_init(plugin_state);
+
+    ValueMutex state_mutex;
+    if(!init_mutex(&state_mutex, plugin_state, sizeof(PluginState))) {
+        FURI_LOG_E("hi", "cannot create mutex\r\n");
+        free(plugin_state);
+        return 255;
+    }
+
+    // Set system callbacks
+    ViewPort* view_port = view_port_alloc();
+    view_port_draw_callback_set(view_port, render_callback, &state_mutex);
+    view_port_input_callback_set(view_port, input_callback, event_queue);
+
+    // Open GUI and register view_port
+    Gui* gui = furi_record_open("gui");
+    gui_add_view_port(gui, view_port, GuiLayerFullscreen);
+
+    PluginEvent event;
+    for(bool processing = true; processing;) {
+        FuriStatus event_status = furi_message_queue_get(event_queue, &event, 100);
+
+        PluginState* plugin_state = (PluginState*)acquire_mutex_block(&state_mutex);
+
+        if(event_status == FuriStatusOk) {
+            // press events
+            if(event.type == EventTypeKey) {
+                if(event.input.type == InputTypePress) {
+                    switch(event.input.key) {
+                    case InputKeyUp:
+                        plugin_state->y--;
+                        break;
+                    case InputKeyDown:
+                        plugin_state->y++;
+                        break;
+                    case InputKeyRight:
+                        plugin_state->x++;
+                        break;
+                    case InputKeyLeft:
+                        plugin_state->x--;
+                        break;
+                    case InputKeyOk:
+                    case InputKeyBack:
+                        processing = false;
+                        break;
+                    }
+                }
+            }
+        } else {
+            FURI_LOG_D("hi", "FuriMessageQueue: event timeout");
+            // event timeout
+        }
+
+        view_port_update(view_port);
+        release_mutex(&state_mutex, plugin_state);
+    }
+
+    view_port_enabled_set(view_port, false);
+    gui_remove_view_port(gui, view_port);
+    furi_record_close("gui");
+    view_port_free(view_port);
+    furi_message_queue_free(event_queue);
+    delete_mutex(&state_mutex);
+
+    return 0;
 }
